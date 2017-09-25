a82d002c5d9d8e9ae7831c0bc115b5ac4e8d2fef
rahmadmartin <rahmad.martin@gmail.com>
2017-09-25 22:48:18 +0700

    #Help, fail ellipse :(

--- ProgrammingAssignment3/Form1.vb
+++ ProgrammingAssignment3/Form1.vb
@@ -46,9 +46,55 @@
         PictureBox1.Image = canvas
     End Sub
 
+    Sub createEllipse(xc As Integer, yc As Integer, a As Integer, b As Integer)
+        Dim x = 0
+        Dim y = b
+        Dim a2 = a * a
+        Dim b2 = b * b
+        Dim d1 = 4 * b2 - 4 * a2 * b + a2
+        canvas.SetPixel(xc + x, yc + y, Color.Black)
+        canvas.SetPixel(xc - x, yc + y, Color.Black)
+        canvas.SetPixel(xc + x, yc - y, Color.Black)
+        canvas.SetPixel(xc - x, yc - y, Color.Black)
+        canvas.SetPixel(xc + y, yc + x, Color.Black)
+        canvas.SetPixel(xc - y, yc + x, Color.Black)
+        canvas.SetPixel(xc + y, yc - x, Color.Black)
+        canvas.SetPixel(xc - y, yc - x, Color.Black)
+
+        While 2 * b2 * (x + 1) >= a2 * (2 * y - 1)
+            x = x + 1
+            If d1 > 0 Then
+                y = y - 1
+                d1 = d1 + b2 * (8 * x + 12) + a2 * (8 - 8 * y)
+            Else
+                d1 = d1 + b2 * (8 * x + 12)
+            End If
+        End While
+
+        Dim d2 = b2 * (2 * x + 1) * 2 + 4 * a2 * (y - 1) * 2 - 4 * a2 * b2
+        While y > 0
+            y = y - 1
+            If d2 < 0 Then
+                x = x + 1
+                d2 = d2 + b2 * (8 * x + 8) + a2 * (12 - 8 * y)
+            Else
+                d2 = d2 + a2 * (12 - 8 * y)
+            End If
+            canvas.SetPixel(xc + x, yc + y, Color.Black)
+            canvas.SetPixel(xc - x, yc + y, Color.Black)
+            canvas.SetPixel(xc + x, yc - y, Color.Black)
+            canvas.SetPixel(xc - x, yc - y, Color.Black)
+            canvas.SetPixel(xc + y, yc + x, Color.Black)
+            canvas.SetPixel(xc - y, yc + x, Color.Black)
+            canvas.SetPixel(xc + y, yc - x, Color.Black)
+            canvas.SetPixel(xc - y, yc - x, Color.Black)
+        End While
+        PictureBox1.Image = canvas
+    End Sub
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         canvas = New Bitmap(PictureBox1.Width, PictureBox1.Height)
         clearCanvas()
         createCircle(100, 100, 20)
+        createEllipse(50, 50, 75, 25)
     End Sub
 End Class
